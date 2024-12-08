fb2ae5e46c5bee6b8da3073199a1d0efd8f6a831
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-08 18:38:31 -0500

    add database connection setup using Sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
+import db from './config/db';
 
 const server = express();
+
+const connectDB = async () => {
+	try {
+		await db.authenticate();
+		db.sync();
+		console.log('La conexion a sido exitosa!!');
+	} catch (error) {
+		console.log(error);
+		console.log('Hubo un error al conectar a la DB');
+	}
+};
+
+connectDB();
 
 export default server;
