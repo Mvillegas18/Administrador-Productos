b8bda9ff6f425069a11c13017c69886279281768
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-08 18:12:57 -0500

    add product router to API server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
+import router from './router';
 import server from './server';
+
+server.use('/api/products', router);
 
 const PORT = process.env.PORT;
 server.listen(3000, () => {
