748ab032efcb4787586803da5a07e63600a040bc
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-09 16:31:30 -0500

    refactor database connection to use sequelize-typescript and specify model paths

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
-const db = new Sequelize(`${process.env.EXTERNAL_DB}`);
+const db = new Sequelize(`${process.env.EXTERNAL_DB}`, {
+	models: [__dirname + '/../models/**/*.ts'],
+});
 
 export default db;
