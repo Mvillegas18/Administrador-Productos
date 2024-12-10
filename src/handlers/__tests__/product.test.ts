a110324dc46c4fe1a304a1cf898926741c6cfa50
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-10 18:48:28 -0500

    add unit test for GET /api/products endpoint to verify JSON response and status

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -41,3 +41,18 @@ describe('POST /api/products', () => {
 		expect(response.body).not.toHaveProperty('errors');
 	});
 });
+
+describe('GET /api/products', () => {
+	it('Get a json response with products', async () => {
+		const response = await request(server).get('/api/products');
+
+		expect(response.status).toBe(200);
+		expect(response.headers['content-type']).toMatch(/json/);
+		expect(response.body).toHaveProperty('data');
+		expect(response.body.data).toBeTruthy();
+
+		expect(response.status).not.toBe(404);
+		expect(response.body.data).toBeTruthy();
+		expect(response.body).not.toHaveProperty('errors');
+	});
+});
