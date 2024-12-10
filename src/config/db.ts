40b5e736eccdbc8b2156cb5aa44e8f1b3cc548df
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-10 14:59:09 -0500

    disable logging for Sequelize database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 const db = new Sequelize(`${process.env.EXTERNAL_DB}`, {
 	models: [__dirname + '/../models/**/*.ts'],
+	logging: false,
 });
 
 export default db;
