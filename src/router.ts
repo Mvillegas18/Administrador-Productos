32c58c428fe920fb1c4d15b8739779ec0dfbdf68
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-09 20:53:53 -0500

    implement product routes with validation and error handling

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,26 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
-import { createProduct } from './handlers/product';
+import { body, param } from 'express-validator';
+import {
+	createProduct,
+	deleteProduct,
+	getProduct,
+	getProductID,
+	updateAvailability,
+	updateProduct,
+} from './handlers/product';
+import { handleImportErrors } from './middleware';
 
 const router = Router();
 
-router.get('/', () => {});
+router.get('/', getProduct);
+
+router.get(
+	'/:id',
+
+	param('id').isInt().withMessage('ID no valido'),
+	handleImportErrors,
+	getProductID
+);
 
 router.post(
 	'/',
@@ -18,19 +34,31 @@ router.post(
 		.custom((value) => value > 0)
 		.withMessage('El precio debe ser mayor a 0'),
 
+	handleImportErrors,
 	createProduct
 );
 
-router.put('/', (req, res) => {
-	res.json({ message: 'Desde PUT' });
-});
+router.put(
+	'/:id',
 
-router.patch('/', (req, res) => {
-	res.json({ message: 'Desde PATCH' });
-});
+	body('name').notEmpty().withMessage('El nombre del producto es obligatorio'),
+	body('price')
+		.isNumeric()
+		.withMessage('Valor no valido')
+		.notEmpty()
+		.withMessage('El precio del producto es obligatorio')
+		.custom((value) => value > 0)
+		.withMessage('El precio debe ser mayor a 0'),
+	body('availability')
+		.isBoolean()
+		.withMessage('Valor para disponibilidad no valido'),
+	handleImportErrors,
 
-router.delete('/', (req, res) => {
-	res.json({ message: 'Desde DELETE' });
-});
+	updateProduct
+);
+
+router.patch('/', updateAvailability);
+
+router.delete('/:id', deleteProduct);
 
 export default router;
