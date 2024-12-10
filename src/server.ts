193fa9da47a2c6f57f74e9441ce947b0411ea093
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-10 15:32:38 -0500

    refactor server setup to correctly initialize and configure Express instance

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,21 @@ import express from 'express';
 import db from './config/db';
 import router from './router';
 
-export const server = express();
-server.use(express.json());
-server.use('/api/products', router);
-
 const connectDB = async () => {
 	try {
 		await db.authenticate();
 		db.sync();
-		console.log(colors.blue.bold('La conexion a sido exitosa!!'));
+		// console.log(colors.blue.bold('La conexion a sido exitosa!!'));
 	} catch (error) {
-		// console.log(error);
-
 		console.log(colors.red.bold('Hubo un error al conectar a la DB'));
 	}
 };
 
 connectDB();
+
+const server = express();
+server.use(express.json());
+server.use('/api/products', router);
 
 server.get('/api/', (req, res) => {
 	res.json({ message: 'desde api' });
