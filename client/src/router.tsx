d0073afe96231dced9acb744af834f16f3c3c876
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-17 20:51:28 -0500

    add routing for product views: include NewProduct and Product components

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,4 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import NewProduct from './views/NewProduct';
+import Product from './views/Product';
 
-export const router = createBrowserRouter([{ path: '/', element: <Layout /> }]);
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <Product />,
+			},
+			{
+				path: 'products/new',
+				element: <NewProduct />,
+			},
+		],
+	},
+]);
