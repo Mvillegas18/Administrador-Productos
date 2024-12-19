3381886e0b172e5fec225f72705ad1cc8c323669
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-18 20:40:45 -0500

    update NewProduct and Product components: change headings for clarity and adjust link button sizes

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function NewProduct() {
 	return (
 		<div className='flex justify-between items-center'>
-			<h2 className='text-4xl font-black text-slate-700'>NewProduct</h2>
+			<h2 className='text-4xl font-black text-slate-700'>Register products</h2>
 			<Link
 				to={'/'}
-				className='bg-slate-700 text-white px-4 py-2 rounded-lg hover:bg-slate-600 transition-all text-lg'>
+				className='bg-slate-700 text-white px-4 py-2 rounded-lg hover:bg-slate-600 transition-all text-sm'>
 				Go to products
 			</Link>
 		</div>
--- client/src/views/Product.tsx
+++ client/src/views/Product.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function Product() {
 	return (
 		<div className='flex justify-between items-center'>
-			<h2 className='text-4xl font-black text-slate-700'>Productos</h2>
+			<h2 className='text-4xl font-black text-slate-700'>Products</h2>
 			<Link
 				to={'/products/new'}
-				className='bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-500 transition-all text-lg'>
+				className='bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-500 transition-all text-sm'>
 				Add product
 			</Link>
 		</div>
