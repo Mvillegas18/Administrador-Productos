1586208ac188cd5c802ccf0448f7bf4184caa7a5
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-18 22:03:19 -0500

    update Layout component: enhance main section styling with shadow-lg class

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout() {
 				</div>
 			</header>
 
-			<main className='mt-10 mx-auto max-w-6xl p-10 shadow bg-white'>
+			<main className='mt-10 mx-auto max-w-6xl p-10 shadow-lg bg-white'>
 				<Outlet />
 			</main>
 		</>
