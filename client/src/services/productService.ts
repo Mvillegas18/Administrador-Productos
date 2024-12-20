36ab611f6bf1631f9c8ee69a41af7885ef1f8e37
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-20 14:39:10 -0500

    refactor: simplify addProduct function by removing unnecessary variable declaration

--- client/src/services/productService.ts
+++ client/src/services/productService.ts
@@ -13,7 +13,7 @@ export const addProduct = async (data: ProductData) => {
 		if (result.success) {
 			const url = `${import.meta.env.VITE_API_URL}/api/products`;
 
-			const data = await axios.post(url, {
+			await axios.post(url, {
 				name: result.output.name,
 				price: result.output.price,
 			});
