de800ea6acda1f45728f0aa25ad9fc32ac691e82
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-26 14:52:44 -0500

    feat: update Sequelize model path to include TypeScript files

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize-typescript';
 
 const db = new Sequelize(`${process.env.EXTERNAL_DB}`, {
-	models: [__dirname + '/../models/**/*'],
+	models: [__dirname + '/../models/**/*.ts'],
 	logging: false,
 });
 
